24fc1a6bee3ccdb94ead076b09456123584e39da
ogiogi93 <mail_ogiwara93@dx5.so-net.ne.jp>
2018-01-03 17:53:31 +0900

    間違ってた

--- recod_web/settings.py
+++ recod_web/settings.py
@@ -114,9 +114,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = (
-    os.path.join(BASE_DIR, "sitestatic"),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, "sitestatic")
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
