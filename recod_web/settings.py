f3f94570c6536bda3c0306a771580f7461d4e851
ogiogi93 <mail_ogiwara93@dx5.so-net.ne.jp>
2018-01-07 15:14:08 +0900

    path 変更

--- recod_web/settings.py
+++ recod_web/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/var/www/recod_web/static'
+STATIC_ROOT = '/var/www/recod_web/staticfiles'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
