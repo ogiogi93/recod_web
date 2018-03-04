b50e177cb42638195ab99a6810be413cc991f318
ogiogi93 <mail_ogiwara93@dx5.so-net.ne.jp>
2018-03-04 16:42:04 +0900

    動画ページもDB見るように

--- video/views.py
+++ video/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 
 from account.forms import login_form
-from article.views import get_new_articles
+from article.entity import GameEntity
 from match.views import get_new_matches, get_next_matches
+from service_api.models.videos import Video
+from service_api.models.disciplines import Game
 
 
 def video_list(request):
+    enabled_games = Game.objects.select_related('discipline').filter(is_active=True)
     return render(request, 'web/video/video_list.html', context={
         'login_form': login_form,
-        'articles': get_new_articles(),
-        'next_matches': get_next_matches()
+        'next_matches': get_next_matches(),
+        'games': [GameEntity(eg) for eg in enabled_games],
+        'default_select_game_id': 2,
+        'videos': Video.objects.select_related('attribute', 'attribute__game').filter(
+            attribute__game__id__in=set(eg.id for eg in enabled_games))
     })
 
 
