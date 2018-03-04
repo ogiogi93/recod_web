27dfce878db048316147e2b7d560981749b9cee4
ogiogi93 <mail_ogiwara93@dx5.so-net.ne.jp>
2018-03-05 00:40:54 +0900

    STG用の設定ファイルを追加

---
+++ recod_web/settings_staging.py
@@ -0,0 +1,8 @@
+from .settings import *
+
+STATIC_URL = 'http://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'recod_web.storage_backends.MediaStorage'
+
+DEBUG = False
+ENV = 'develop'
