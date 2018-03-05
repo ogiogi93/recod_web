98a074de6786237b6e0d9c2caf2db7ad095d2971
ogiogi93 <mail_ogiwara93@dx5.so-net.ne.jp>
2018-03-05 23:33:32 +0900

    仮にstgの設定にしておく

--- recod_web/wsgi.py
+++ recod_web/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "recod_web.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "recod_web.settings_staging")
 
 application = get_wsgi_application()
