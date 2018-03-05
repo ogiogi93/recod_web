02df893f65395271efb500127a33050dd7c45bb1
ogiogi93 <mail_ogiwara93@dx5.so-net.ne.jp>
2018-03-05 10:22:31 +0900

    not .com

--- recod_web/settings_production.py
+++ recod_web/settings_production.py
@@ -1,6 +1,6 @@
 from .settings import *
 
-STATIC_ROOT = '/var/www/recod_web.com/staticfiles/'
+STATIC_ROOT = '/var/www/recod_web/staticfiles/'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'recod_web.storage_backends.MediaStorage'
 
